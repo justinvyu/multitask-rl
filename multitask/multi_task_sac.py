82eafa70a6fd2b1e09ea92682d4fedad6fdb6a17
justinvyu <justin.v.yu@gmail.com>
2019-04-22 01:07:38 -0700

    Add general env size

--- multitask/multi_task_sac.py
+++ multitask/multi_task_sac.py
@@ -1,4 +1,5 @@
 from gym_pointmass.envs import PointMassEnv
+from gym_pointmass.envs import ClippedPointMassEnv
 from multiworld.core.flat_goal_env import FlatGoalEnv
 
 import rlkit.torch.pytorch_util as ptu
@@ -10,6 +11,33 @@ from rlkit.torch.sac.policies import TanhGaussianPolicy, MakeDeterministic
 from rlkit.torch.sac.sac import SACTrainer
 from rlkit.torch.networks import FlattenMlp
 from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+class ClippedTanhGaussianPolicy(TanhGaussianPolicy):
+    def __init__(self, obs_dim, action_dim, hidden_sizes, clip=0.1):
+        super(ClippedTanhGaussianPolicy, self).__init__(
+            obs_dim=obs_dim,
+            action_dim=action_dim,
+            hidden_sizes=hidden_sizes
+        )
+        self.clip = clip
+
+    def forward(
+        self,
+        obs,
+        reparameterize=True,
+        deterministic=False,
+        return_log_prob=False,
+    ):
+        output = list(super(ClippedTanhGaussianPolicy, self).forward(obs=obs,
+                                                                reparameterize=reparameterize,
+                                                                deterministic=deterministic,
+                                                                return_log_prob=return_log_prob))
+        output[0] = output[0] * self.clip
+        return output
 
 def experiment(variant):
     base_expl_env = PointMassEnv(n=variant["num_tasks"])
@@ -29,27 +57,27 @@ def experiment(variant):
     qf1 = FlattenMlp(
         input_size=obs_dim + action_dim,
         output_size=1,
-        hidden_sizes=[M, M],
+        hidden_sizes=[M, M, M],
     )
     qf2 = FlattenMlp(
         input_size=obs_dim + action_dim,
         output_size=1,
-        hidden_sizes=[M, M],
+        hidden_sizes=[M, M, M],
     )
     target_qf1 = FlattenMlp(
         input_size=obs_dim + action_dim,
         output_size=1,
-        hidden_sizes=[M, M],
+        hidden_sizes=[M, M, M],
     )
     target_qf2 = FlattenMlp(
         input_size=obs_dim + action_dim,
         output_size=1,
-        hidden_sizes=[M, M],
+        hidden_sizes=[M, M, M],
     )
     policy = TanhGaussianPolicy(
         obs_dim=obs_dim,
         action_dim=action_dim,
-        hidden_sizes=[M, M],
+        hidden_sizes=[M, M, M],
     )
     eval_policy = MakeDeterministic(policy)
     eval_path_collector = MdpPathCollector(
@@ -90,10 +118,10 @@ if __name__ == "__main__":
         algorithm="SAC",
         version="normal",
         layer_size=64,
-        num_tasks=25,
-        replay_buffer_size=int(1E6),
+        num_tasks=1,
+        replay_buffer_size=int(1e5),
         algorithm_kwargs=dict(
-            num_epochs=500,
+            num_epochs=400,
             num_eval_steps_per_epoch=1000,
             num_trains_per_train_loop=200,
             num_expl_steps_per_train_loop=200,
--- multitask/run.py
+++ multitask/run.py
@@ -7,13 +7,10 @@ import uuid
 from rlkit.core import logger
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
-if __name__ == "__main__":
-    # file = "./logs/sac-pointmass-multitask/sac-pointmass-multitask_2019_04_13_23_32_53_0000--s-0/params.pkl"
-    # file = "./logs/sac-pointmass-multitask-3/sac-pointmass-multitask-3_2019_04_14_00_30_43_0000--s-0/params.pkl"
-    # file = "./logs/sac-pointmass-multitask-5/sac-pointmass-multitask-5_2019_04_14_01_40_18_0000--s-0/params.pkl"
-    # file = "./logs/sac-pointmass-multitask-10/sac-pointmass-multitask-10_2019_04_14_13_54_28_0000--s-0/params.pkl"
-    file = "./logs/sac-pointmass-multitask-25/sac-pointmass-multitask-25_2019_04_15_23_30_06_0000--s-0/params.pkl"
+def plot_separated_by_task(file):
+    file = "./logs/sac-pointmass-multitask-5/sac-pointmass-multitask-5_2019_04_20_18_57_19_0000--s-0/params.pkl"
 
     data = joblib.load(file)
     policy = data['evaluation/policy']
@@ -51,7 +48,7 @@ if __name__ == "__main__":
         goal_plot.scatter(start_x, start_y, color="green")
         goal_plot.scatter(obs[1:, 0], obs[1:, 1], color="b")
         goal_plot.quiver(obs[:, 0], obs[:, 1], acts[:, 0], acts[:, 1],
-                   angles='xy', scale_units='xy', scale=1, width=.005, headwidth=3, alpha=.9)
+                         angles='xy', scale_units='xy', scale=1, width=.005, headwidth=3, alpha=.9)
         # plt.annotate("start=({0}, {1})".format(start_x.round(4), start_y.round(4)), (start_x, start_y), xytext=(start_x-.5, start_y+.2))
 
         final_x, final_y = obs[len(obs) - 1, 0], obs[len(obs) - 1, 1]
@@ -60,26 +57,94 @@ if __name__ == "__main__":
         goal = env.goals[goal_idx]
         goal_x, goal_y = goal[0], goal[1]
         # plt.annotate("goal=({0}, {1})".format(goal_x.round(4), goal_y.round(4)), (goal_x, goal_y), xytext=(goal_x-.5, goal_y+.1))
-        goal_plot.scatter(goal[0], goal[1], color="r") # Goal
+        goal_plot.scatter(goal[0], goal[1], color="r")  # Goal
         goal_plot.set_xlim(-1.5, 1.5)
         goal_plot.set_ylim(-1.5, 1.5)
-
 
         if not has_circle[goal_idx]:
             circle = plt.Circle((0, 0), 1, color='black', alpha=.5, fill=False)
             goal_plot.add_artist(circle)
             has_circle[goal_idx] = True
 
-        # rew = path["rewards"]
-        # plt.plot(rew)
+if __name__ == "__main__":
+    sns.set()
 
+    # file = "./logs/sac-pointmass-multitask/sac-pointmass-multitask_2019_04_13_23_32_53_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-3/sac-pointmass-multitask-3_2019_04_14_00_30_43_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-5/sac-pointmass-multitask-5_2019_04_14_01_40_18_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-10/sac-pointmass-multitask-10_2019_04_14_13_54_28_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-25/sac-pointmass-multitask-25_2019_04_15_23_30_06_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-5/sac-pointmass-multitask-5_2019_04_20_18_57_19_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-1/sac-pointmass-multitask-1_2019_04_20_20_39_43_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-1/sac-pointmass-multitask-1_2019_04_20_22_37_49_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-1/sac-pointmass-multitask-1_2019_04_20_22_55_55_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-1/sac-pointmass-multitask-1_2019_04_20_23_13_09_0000--s-0/params.pkl"
+
+    # Trained for fewer timesteps
+    file = "./logs/sac-pointmass-multitask-1/sac-pointmass-multitask-1_2019_04_21_00_44_12_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-2/sac-pointmass-multitask-2_2019_04_21_01_11_43_0000--s-0/params.pkl"
+    # file = "./logs/sac-pointmass-multitask-1/sac-pointmass-multitask-1_2019_04_21_12_16_24_0000--s-0/params.pkl"
+
+    data = joblib.load(file)
+    policy = data['evaluation/policy']
+    env = data['evaluation/env']
+
+    plt.figure(figsize=(8, 8))
+    num_goals = len(env.goals)
+
+    print("Number of goals:", num_goals)
+    for i in range(25):
+        path = rollout(
+            env,
+            policy,
+            max_path_length=100,
+            animated=False,
+        )
+
+        # print(path)
+        obs = path["observations"]
+        acts = path["actions"]
+        goal_idx = np.argmax(obs[0, 2:])
+        plot_row, plot_col = goal_idx // 5, goal_idx % 5
+
+        start_x = obs[0, 0]
+        start_y = obs[0, 1]
+
+        plt.scatter(start_x, start_y, color="green")
+        plt.scatter(obs[1:, 0], obs[1:, 1], color="b", s=10)
+
+        acts_x = acts[:, 0]
+        acts_y = acts[:, 1]
+
+        # norms = np.linalg.norm(acts, axis=1)
+        # acts_x = acts[:, 0] / norms * 0.1
+        # acts_y = acts[:, 1] / norms * 0.1
+
+        plt.quiver(obs[:, 0], obs[:, 1], acts_x, acts_y,
+                   angles='xy', scale_units='xy', scale=1, width=.002, headwidth=2, alpha=.9)
+        # plt.quiver(obs[:, 0], obs[:, 1], acts[:, 0], acts[:, 1],
+        #            angles='xy', scale_units='xy', scale=1, width=.005, headwidth=3, alpha=.9)
+        # plt.annotate("start=({0}, {1}), {2}".format(start_x.round(4), start_y.round(4), i), (start_x, start_y), xytext=(start_x-.5, start_y+.2))
+
+        final_x, final_y = obs[len(obs) - 1, 0], obs[len(obs) - 1, 1]
+        # plt.scatter(final_x, final_y)
+        plt.annotate("end=({0}, {1})".format(final_x.round(4), final_y.round(4)), (final_x, final_y),
+                     xytext=(final_x - .5, final_y - 0.5))
+
+        goal = env.goals[goal_idx]
+        goal_x, goal_y = goal[0], goal[1]
+        # plt.annotate("goal=({0}, {1})".format(goal_x.round(4), goal_y.round(4)), (goal_x, goal_y), xytext=(goal_x-.5, goal_y+.1))
+        plt.scatter(goal[0], goal[1], color="r") # Goal
+
+        plt.xlim(-env.bound, env.bound)
+        plt.ylim(-env.bound, env.bound)
 
     # Legend
-    # plt.legend(["Initial State (s_0)", "States (s_t)", "Actions (a_t)", "Goal Point"])
+    plt.legend(["Initial State (s_0)", "States (s_t)", "Actions (a_t)", "Goal Point"])
 
     # Add unit circle
-    # circle = plt.Circle((0, 0), 1, color='black', alpha=.5, fill=False)
-    # plt.gcf().gca().add_artist(circle)
+    circle = plt.Circle((0, 0), env.goal_distance, color='black', alpha=.5, fill=False)
+    plt.gcf().gca().add_artist(circle)
 
     plt.show()
 
