51045e7581d29ecbc7dbfc5e5286b3bb42fcfa05
justinvyu <justin.v.yu@gmail.com>
2019-04-23 15:05:17 -0700

    Add run_policy file

---
+++ multitask/run_policy.py
@@ -0,0 +1,52 @@
+
+from rlkit.samplers.util import rollout
+import joblib
+import numpy as np
+
+def run_policy(fn, num_rollouts):
+    """
+    Takes in a trained policy, runs the policy for a specified number of
+    rollouts, and returns the results of the experiment.
+    :param fn: The path to the `params.pkl` file containing the trained policy.
+    :param num_rollouts: The number of rollouts to experience.
+    :return: A list of the `num_rollouts` recorded paths to be used for
+             further analysis.
+    """
+    data = joblib.load(fn)
+
+    # Load deterministic evaluation policy.
+    policy = data["evaluation/policy"]
+    env = data["evaluation/env"]
+
+    start_states, final_states, goal_states, actions, paths = [], [], [], [], []
+
+    for i in range(num_rollouts):
+        path = rollout(
+            env,
+            policy,
+            max_path_length=100,
+            animated=False,
+        )
+        obs = path["observations"]
+        acts = path["actions"]
+
+        goal_idx = np.argmax(obs[0, 2:])
+        start_x, start_y = obs[0, 0], obs[0, 1]
+        acts_x, acts_y = acts[:, 0], acts[:, 1]
+        final_x, final_y = obs[len(obs) - 1, 0], obs[len(obs) - 1, 1]
+        goal = env.goals[goal_idx]
+        goal_x, goal_y = goal[0], goal[1]
+
+        start_states.append(np.array([start_x, start_y]))
+        final_states.append(np.array([final_x, final_y]))
+        goal_states.append(np.array([goal_x, goal_y]))
+        actions.append(np.array([acts_x, acts_y]))
+        paths.append(path)
+
+    return dict(
+        start_states=start_states,
+        final_states=final_states,
+        goal_states=goal_states,
+        actions=actions,
+        paths=paths
+    )
